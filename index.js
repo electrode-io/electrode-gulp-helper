032eef479f23d181e03f09779e5e4c8f8b310a9b
Joel Chen <xchen@walmartlabs.com>
2016-09-23 17:36:57 -0700

    export shell

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const loadTasks = require("./lib/load-tasks");
 const exec = require("./lib/exec");
 const envPath = require("./lib/env-path");
+const shell = require("shelljs");
 
 module.exports = {
   loadTasks,
   exec,
-  envPath
+  envPath,
+  shell
 };
